0300c710b3868e6b3fc6072bc1bd82656217522d
b-kkoo <bmcow331@naver.com>
2024-11-14 04:35:10 +0900

    feat: 전진 시 RandomGenerator 활성화

--- Assets/Scripts/Manager/Manager.cs
+++ Assets/Scripts/Manager/Manager.cs
@@ -71,7 +71,7 @@ public class Manager : MonoBehaviour
     {
         currentDistance += 1;
         distance?.Invoke(currentDistance);
-        //levelGenerator.RandomGenerator();
+        levelGenerator.RandomGenerator();
     }
 
     public void GameOver()
